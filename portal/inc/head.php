06a1f7758943382823b09075bbaba37fd04f2687
Andrés Puppo <andres@puppo.uy>
2026-07-03 13:08:30 -0300

    Add robots.txt and update meta tags to prevent search engine indexing

--- portal/inc/head.php
+++ portal/inc/head.php
@@ -4,8 +4,8 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="robots" content="noindex">
-  <meta name="googlebot" content="noindex">
+  <meta name="robots" content="noindex, nofollow">
+  <meta name="googlebot" content="noindex, nofollow">
 
   <title><?php echo $title; ?> | Sistema de Diseño</title>
 
