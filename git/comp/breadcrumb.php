36ba2478d1fff90f05009acb4b40b8fc57deb98b
Andrés Puppo <andres@puppo.uy>
2026-07-13 09:55:12 -0300

    docs: corregir warning de variable  indefinida en breadcrumb.php

--- git/comp/breadcrumb.php
+++ git/comp/breadcrumb.php
@@ -2,6 +2,6 @@
 	<ul>
 		<li><a href="#">Inicio</a></li>
 		<li><a href="#">Sistema de diseño</a></li>
-		<li><?php echo $title; ?></li>
+		<li><?php echo $title ?? 'Página actual'; ?></li>
 	</ul>
 </div>	
