67c54ff63469a2a54f1c2eb1a011c3ec8ff982ec
Andrés Puppo <andres@puppo.uy>
2026-07-23 19:35:22 -0300

    Ajsutes rollback botone

--- git/comp/botones/boton-primario.php
+++ git/comp/botones/boton-primario.php
@@ -1,7 +1,52 @@
 <button class="boton boton--primario">
+	<span>Botón primario</span>
+</button>
+
+<button class="boton boton--primario boton--s">
+	<svg class="icono">
+		<use href="#icono-co-editor--lineal"></use>
+	</svg>
+	<span>Botón primario S</span>
+</button>
+
+<button class="boton boton--primario">
 	<svg class="icono">
 		<use href="#icono-co-editor--lineal"></use>
 	</svg>
 	<span>Botón primario</span>
 </button>
 
+<button class="boton boton--primario boton--l">
+	<svg class="icono">
+		<use href="#icono-co-editor--lineal"></use>
+	</svg>
+	<span>Botón primario L</span>
+</button>
+
+<button class="boton boton--primario boton--icono boton--s">
+	<svg class="icono">
+		<use href="#icono-co-editor--lineal"></use>
+	</svg>
+	<span>Acción botón primario</span>
+</button>
+
+<button class="boton boton--primario boton--icono">
+	<svg class="icono">
+		<use href="#icono-co-editor--lineal"></use>
+	</svg>
+	<span>Acción botón primario</span>
+</button>
+
+<button class="boton boton--primario boton--icono boton--l">
+	<svg class="icono">
+		<use href="#icono-co-editor--lineal"></use>
+	</svg>
+	<span>Acción botón primario</span>
+</button>
+
+<button class="boton boton--primario" disabled>
+	<svg class="icono">
+		<use href="#icono-co-editor--lineal"></use>
+	</svg>
+	<span>Botón primario</span>
+</button>
