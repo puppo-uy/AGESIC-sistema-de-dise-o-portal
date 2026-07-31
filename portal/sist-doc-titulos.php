475f372d870dd9886ee516b242f597de6469829b
Andrés Puppo <andres@puppo.uy>
2026-07-31 19:57:13 -0300

    prueba cambio ruta frame

--- portal/sist-doc-titulos.php
+++ portal/sist-doc-titulos.php
@@ -44,7 +44,7 @@
                 <p class="Page-description">Los títulos son componentes de texto que se utilizan para organizar y jerarquizar el contenido dentro de la interfaz. Permiten estructurar la información, guiar la lectura y facilitar la comprensión del contenido en distintos niveles.</p>
                 <p>Además de su función visual, cumplen un rol semántico fundamental, ya que definen la estructura lógica del contenido.</p>
                 
-                 <iframe src="../git/iframe-preview.php?comp=texto/titulos-todos" title="Ejemplos de títulos del sistema" class="component-preview u-mb3" style="width: 100%; border: none; min-height: 380px;" scrolling="no"></iframe>
+                 <iframe src="../biblioteca/iframe-preview.php?comp=texto/titulos-todos" title="Ejemplos de títulos del sistema" class="component-preview u-mb3" style="width: 100%; border: none; min-height: 380px;" scrolling="no"></iframe>
 
                 
                 <h3>Recomendaciones de uso</h3>
